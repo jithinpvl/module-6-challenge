700ce11230c9caa77d094869c171a4a949f6cdcb
Jithin Chandran <jithinthekkemalaparambil@Jithins-MBP.broadband>
2023-09-01 18:56:49 +0100

    homework

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+let questions = [
+    {
+        "title": "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+        "choices":["While Loop","Else Loop","For Loop","Conditional Loop"],
+        "answer": "While Loop"
+    },
+    {
+        
+        "title": "What is the object called that lets you work with both dates and time-related data?",
+        "choices":["Clock","Dates","Time field","Time-warp"],
+        "answer": "Dates"
+
+        
+    },
+    {
+        "title": "In JavaScript, what is a block of code called that is used to perform a specific task?",
+        "choices":["String","Variable","Declaration","Function"],
+        "answer": "Function"
+
+    },
+
+    {
+        "title": "In JavaScript, what element is used to store and manipulate text usually in multiples?",
+        "choices":["Function","Arrays","Variables","Strings"],
+        "answer": "Strings"
+    }
+
+        
+]
